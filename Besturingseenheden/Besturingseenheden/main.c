61bf70b87d662539a643333af8ca427346e860f5
Mike Clarke <mikec655@gmail.com>
2018-10-10 11:48:47 +0200

    Atmel project aangemaakt

---
+++ Besturingseenheden/Besturingseenheden/main.c
@@ -0,0 +1,18 @@
+/*
+ * Besturingseenheden.c
+ *
+ * Created: 10-Oct-18 11:47:42
+ * Author : 
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
