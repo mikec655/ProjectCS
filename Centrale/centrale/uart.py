8d7fa8739bf13faf94ff988d3a37d7dab51306e0
Mike Clarke <mikec655@gmail.com>
2018-10-13 22:41:03 +0200

    Er kan nu 1 sensor worden uitgelezen en over serial verstuurd worden.

--- Centrale/centrale/uart.py
+++ Centrale/centrale/uart.py
@@ -4,4 +4,12 @@ ser = serial.Serial("COM3", 19200)
 print(ser)
 while True:
     s = ser.read()
-    print(s.hex())
+    value = int.from_bytes(s, byteorder='little', signed=False)
+   
+    '''
+    # test for temperture sensor
+    v_out = value * (5.0 / 256)
+    celsius = (v_out - 0.5) * 100
+    print(celsius)
+    '''
+    print(value)
