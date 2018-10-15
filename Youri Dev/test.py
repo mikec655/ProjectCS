3e2ce507c2dab720ef25cbbe274174c91c120bf9
Bernt Bosma <berntjanbosma@gmail.com>
2018-10-15 14:06:56 +0200

    'berry'

---
+++ Youri Dev/test.py
@@ -0,0 +1,24 @@
+from matplotlib import pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+
+x = [5,8,10]
+y = [12,16,6]
+
+x2 = [6,9,11]
+y2 = [6,15,7]
+
+
+plt.plot(x,y,'b',label='line one', linewidth=3)
+plt.plot(x2,y2,'y',label='line two',linewidth=3)
+
+plt.title('Epic Info')
+plt.ylabel('Y axis')
+plt.xlabel('X axis')
+
+plt.legend()
+
+plt.grid(True,color='k')
+
+plt.show()
