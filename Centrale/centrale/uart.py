623d51441a9daa1b1d61c1163aad322cc920beb1
Youri <Youri.sanders@hotmail.com>
2018-10-15 13:28:59 +0200

    mike help pliz (write data naar bestand)

--- Centrale/centrale/uart.py
+++ Centrale/centrale/uart.py
@@ -1,15 +1,18 @@
 import serial
-
+i = 0
 ser = serial.Serial("COM3", 19200)
 print(ser)
 while True:
     s = ser.read()
     value = int.from_bytes(s, byteorder='little', signed=False)
    
-    '''
-    # test for temperture sensor
-    v_out = value * (5.0 / 256)
-    celsius = (v_out - 0.5) * 100
-    print(celsius)
-    '''
-    print(value)
+    if value > 0:
+        # test for temperture sensor
+        v_out = value * (5.0 / 256)
+        celsius = round((v_out - 0.5) * 100)
+        
+        with open ("data.txt", "r+") as f:
+            data = f.read()
+            f.write(data)
+            f.write(str(i) + "," + str(celsius) + '\n')
+        i += 1
