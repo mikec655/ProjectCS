6f550bd2806940e14a0ea5ec8bce408ac18506c9
Youri <Youri.sanders@hotmail.com>
2018-10-15 14:12:26 +0200

    Zodra je RealtimePlot in idle runt, uart.py in studiocode of iets. dan update alles en werkt alles

--- Centrale/centrale/RealTimeBasisPlot.py
+++ Centrale/centrale/RealTimeBasisPlot.py
@@ -7,7 +7,7 @@ fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 
 def animate (i) :
-    graph_data = open('data.txt','r').read()
+    graph_data = open("data.txt","r").read()
     lines = graph_data.split('\n')
     xs = []
     ys = []
--- Centrale/centrale/uart.py
+++ Centrale/centrale/uart.py
@@ -11,7 +11,31 @@ while True:
         v_out = value * (5.0 / 256)
         celsius = round((v_out - 0.5) * 100)
         
-        with open ("Centrale/centrale/data.txt", "r+") as f:
+        with open ("data.txt", "r+") as f:
             data = f.read()
             f.write(str(i) + "," + str(celsius) + '\n')
         i += 1
+
+#guiplotdinges
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+def animate (i) :
+    graph_data = open("data.txt","r").read()
+    lines = graph_data.split('\n')
+    xs = []
+    ys = []
+    for line in lines:
+        if len(line) > 1:
+            x, y = line.split(',')
+            xs.append(float(x))
+            ys.append(float(y))
+        ax1.clear()
+        ax1.plot(xs, ys)
+
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
