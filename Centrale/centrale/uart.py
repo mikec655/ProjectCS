094560bcd82f9ec8ea249f249b8c2cc3c6f1453c
Mike Clarke <mikec655@gmail.com>
2018-10-15 13:52:58 +0200

    uart.py slaat data op in data.txt

--- Centrale/centrale/uart.py
+++ Centrale/centrale/uart.py
@@ -1,18 +1,17 @@
 import serial
+
 i = 0
 ser = serial.Serial("COM3", 19200)
 print(ser)
 while True:
     s = ser.read()
     value = int.from_bytes(s, byteorder='little', signed=False)
-   
     if value > 0:
         # test for temperture sensor
         v_out = value * (5.0 / 256)
         celsius = round((v_out - 0.5) * 100)
         
-        with open ("data.txt", "r+") as f:
+        with open ("Centrale/centrale/data.txt", "r+") as f:
             data = f.read()
-            f.write(data)
             f.write(str(i) + "," + str(celsius) + '\n')
         i += 1
