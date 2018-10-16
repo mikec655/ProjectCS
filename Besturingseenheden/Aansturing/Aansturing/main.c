3cf88b4057d6c99f30cae2a9fc904b00647d712f
Mike Clarke <mikec655@gmail.com>
2018-10-16 10:20:03 +0200

    Drie Atmel projecten aangemaakt voor besturingseenheden

---
+++ Besturingseenheden/Aansturing/Aansturing/main.c
@@ -0,0 +1,18 @@
+/*
+ * Aansturing.c
+ *
+ * Created: 16-Oct-18 10:13:07
+ * Author :
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ Besturingseenheden/Lichtsensor/Lichtsensor/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lichtsensor.c
+ *
+ * Created: 16-Oct-18 10:14:02
+ * Author : 
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
