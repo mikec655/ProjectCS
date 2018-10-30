418c111bcef801165a3ff6c9f146104a2b4de464
Bernt Bosma <berntjanbosma@gmail.com>
2018-10-30 12:40:07 +0100

    'inlogscherm'

---
+++ Centrale/centrale/login.py
@@ -0,0 +1,65 @@
+import main as mn
+from tkinter import *
+import os
+import properties as pp
+
+class Login:
+
+    def __init__(self):
+        self.creds = 'tempfile.temp'
+        self.pwordE = ''
+        self.nameE = ''
+        self.loginN = ''
+        self.sNB = ''
+        self.loggedin = False
+
+        with open(self.creds, 'r+') as f:  # Creates a document using the variable we made at the top.
+            f.write('Admin')
+            # self.nameE.get())  # nameE is the variable we were storing the input to. Tkinter makes us use .get() to get the actual string.
+            f.write('\n')  # Splits the line so both variables are on different lines.
+            f.write('Admin1')  # Same as nameE just with pword var
+        with open(self.creds, 'r') as f:
+            initdata = f.readlines()
+            self.nameE = initdata[0].rstrip()  # Data[0], 0 is the first line, 1 is the second and so on.
+            self.pwordE = initdata[1].rstrip()  # Using .rstrip() will remove the \n (new line) word from before when we input it
+            # f.close()  # Closes the file
+
+
+    def frame(self, login, nb):
+        self.loginN = login
+        self.sNB = nb
+        inloggen = Login()
+        instruction = Label(login, text='Please login: ')
+        instruction.grid(sticky=E)
+
+        name = Label(login,text = 'Username: ')
+        passw = Label(login, text='Password')
+
+        name.grid(row=1,sticky=W)
+        passw.grid(row=2,sticky=W)
+
+        nameE = Entry(login)
+        pwordE = Entry(login, show='*')
+        nameE.grid(row=1, column=1)
+        pwordE.grid(row=2, column=1)
+
+        loginB = Button(login, text='Login',command=self.CheckLogin)
+        loginB.grid(columnspan=2, sticky=W)
+
+
+        nb.add(login, text='login')
+
+    def CheckLogin(self):
+        with open(self.creds) as f:
+            data = f.readlines()  # This takes the entire document we put the info into and puts it into the data variable
+            uname = data[0].rstrip()  # Data[0], 0 is the first line, 1 is the second and so on.
+            pword = data[1].rstrip()  # Using .rstrip() will remove the \n (new line) word from before when we input it
+
+        if self.nameE == uname and self.pwordE == pword:  # Checks to see if you entered the correct data.        print('logedin')
+            self.loggedin = True
+            print('correct pass')
+
+
+        else:
+            print('foute loggin')
+
--- Centrale/centrale/main.py
+++ Centrale/centrale/main.py
@@ -1,8 +1,11 @@
 from tkinter import Tk, Label, Entry, Button, Checkbutton
 from tkinter import ttk
 from sensors import Sensor, SerialException, list_ports
+import login
+import properties as prprts
 import time
 import threading
+
 
 
 class Application(Tk):
@@ -12,101 +15,34 @@ class Application(Tk):
         super().__init__()
         self.sensors = []
         self.threads = []
+        self.frames = []
+        self.makeFrame()
+
+    def makeFrame(self):
         self.config(background='white')
         self.geometry("1000x700")
         self.title("Application")
-        nb = ttk.Notebook(self)
-        properties = ttk.Frame(nb)
-        
-        rolluiklabel = Label(properties, text="Rolluik:")
-        rolluiklabel.config(font=("Times new roman", 18))
-        
-        maxuitrollabel = Label(properties, text="Maximale uitrol:")
-        maxuitrolbox = Entry(properties)
+        self.nb = ttk.Notebook(self)
 
-        minuitrollabel = Label(properties, text="Maximale Inrol:")
-        minuitrolbox = Entry(properties)
-
-        grenswaardelabel = Label(properties, text="Grenswaarde")
-        grenswaardebox1 = Entry(properties)
-        grenswaardebox2 = Entry(properties)
-        grenswaardebox3 = Entry(properties)
-        grenswaardebox4 = Entry(properties)
-        grenswaardebox5 = Entry(properties)
-
-        aantimerlabel = Label(properties, text="Automatisch aan")
-        uittimerlabel = Label(properties, text="Automatisch uit")
-
-        aantimerbox = Entry(properties)
-        uittimerbox = Entry(properties)
-
-        aanuitlabel = Label(properties, text="Aan uit")
-
-        sensor1label = Label(properties, text="Licht Sensor")
-        sensor2label = Label(properties, text="Temperatuur Sensor")
-        sensor3label = Label(properties, text="SensorNaam")
-        sensor4label = Label(properties, text="SensorNaam")
-        sensor5label = Label(properties, text="SensorNaam")
-
-        luikopenlabel = Label(properties, text="Rol luik open")
-        luikdichtlabel = Label(properties, text="Rol luik dicht")
-
-        rolluiklabel.grid(row = 0, column = 0, columnspan = 50, padx = 1, pady = 20, sticky = 'w')
-
-        maxuitrollabel.grid(row=5, column=0 , columnspan = 20, padx = 1, pady = 1, sticky = 'w')
-        maxuitrolbox.grid(row = 5, column=30 , columnspan = 20, padx = 1, pady = 1, sticky = 'n')
-
-        minuitrollabel.grid(row=25, column=0 , columnspan = 20, padx = 1, pady = 1, sticky = 'w')
-        minuitrolbox.grid(row = 25, column=30 , columnspan = 20, padx = 1, pady = 1, sticky = 'n')
-
-        sensor1label.grid(row = 55, column = 0, columnspan = 40, padx = 1, pady = 1, sticky = 'w')
-        sensor2label.grid(row = 56, column = 0, columnspan = 40, padx = 1, pady = 1, sticky = 'w')
-        sensor3label.grid(row = 57, column = 0, columnspan = 40, padx = 1, pady = 1, sticky = 'w')
-        sensor4label.grid(row = 58, column = 0, columnspan = 40, padx = 1, pady = 1, sticky = 'w')
-        sensor5label.grid(row = 59, column = 0, columnspan = 40, padx = 1, pady = 1, sticky = 'w')
-
-        aantimerlabel.grid(row = 60, column = 0, columnspan = 40, padx = 1, pady = 1, sticky = 'w')
-        uittimerlabel.grid(row = 61, column = 0, columnspan = 40, padx = 1, pady = 1, sticky = 'w')
-
-        luikopenlabel.grid(row = 62, column = 0, columnspan = 40, padx = 1, pady = 1, sticky = 'w')
-        luikdichtlabel.grid(row = 63, column = 0, columnspan = 40, padx = 1, pady = 1, sticky = 'w')
-
-        grenswaardelabel.grid(row = 50, column = 45, columnspan = 40, padx = 1, pady = 1, sticky = 'w')
-        grenswaardebox1.grid(row = 55, column=45 , columnspan = 20, padx = 1, pady = 1, sticky = 'n')
-        grenswaardebox2.grid(row = 56, column=45 , columnspan = 20, padx = 1, pady = 1, sticky = 'n')
-        grenswaardebox3.grid(row = 57, column=45 , columnspan = 20, padx = 1, pady = 1, sticky = 'n')
-        grenswaardebox4.grid(row = 58, column=45 , columnspan = 20, padx = 1, pady = 1, sticky = 'n')
-        grenswaardebox5.grid(row = 59, column=45 , columnspan = 20, padx = 1, pady = 1, sticky = 'n')
-
-        aantimerbox.grid(row = 60, column=45 , columnspan = 20, padx = 1, pady = 1, sticky = 'n')
-        uittimerbox.grid(row = 61, column=45 , columnspan = 20, padx = 1, pady = 1, sticky = 'n')
-
-        aanuitlabel.grid(row = 50, column = 260, columnspan = 40, padx = 1, pady = 1, sticky = 'w')
-        s1box = Checkbutton(properties)
-        s1box.grid(row = 55, column=280 , columnspan = 40, padx = 1, pady = 1, sticky = 'n')
-        s2box = Checkbutton(properties)
-        s2box.grid(row = 56, column=280 , columnspan = 40, padx = 1, pady = 1, sticky = 'n')
-        s3box = Checkbutton(properties)
-        s3box.grid(row = 57, column=280 , columnspan = 40, padx = 1, pady = 1, sticky = 'n')
-        s4box = Checkbutton(properties)
-        s4box.grid(row = 58, column=280 , columnspan = 40, padx = 1, pady = 1, sticky = 'n')
-        s5box = Checkbutton(properties)
-        s5box.grid(row = 59, column=280 , columnspan = 40, padx = 1, pady = 1, sticky = 'n')
-        s6box = Checkbutton(properties)
-        s6box.grid(row = 60, column=280 , columnspan = 40, padx = 1, pady = 1, sticky = 'n')
-        s7box = Checkbutton(properties)
-        s7box.grid(row = 61, column=280 , columnspan = 40, padx = 1, pady = 1, sticky = 'n')
-        s8box = Checkbutton(properties)
-        s8box.grid(row = 62, column=280 , columnspan = 40, padx = 1, pady = 1, sticky = 'n')
-        s9box = Checkbutton(properties)
-        s9box.grid(row = 63, column=280 , columnspan = 40, padx = 1, pady = 1, sticky = 'n')
-
-        nb.add(properties, text='properties')
+        login0 = ttk.Frame(self.nb)
+        rpropertie = prprts.properties()
+        loginFrame = login.Login()    
+        loginFrame.frame(login0, self.nb)
 
         while True:
             self.update()
-            self.check_for_sensors(nb)
-            nb.pack(expand=1, fill="both")
+            self.check_for_sensors(self.nb)
+            print(loginFrame.loggedin)
+
+            if loginFrame.loggedin == True:
+                print(908876)
+                loginFrame.loggedin = False
+                instellingen = ttk.Frame(self.nb)
+                rpropertie.propertieFrame(self.nb ,instellingen)
+
+            self.nb.pack(expand=1, fill="both")
+
+
 
     def check_for_sensors(self, nb):
         available_ports = list_ports.comports()
