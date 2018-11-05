0c0ac0c7a5d5f27b3d768c019a31a95026b48bf7
gerben bunt <g.bunt@st.hanze.nl>
2018-11-05 10:34:38 +0100

    properties bezig geweest

--- Centrale/centrale/properties.py
+++ Centrale/centrale/properties.py
@@ -1,4 +1,4 @@
-from tkinter import Entry, Label, Checkbutton, Button, StringVar, OptionMenu
+from tkinter import Entry, Label, Checkbutton, Button, StringVar, OptionMenu, ANCHOR
 from tkinter.ttk import Frame
 
 class Properties(Frame):
@@ -7,20 +7,81 @@ class Properties(Frame):
         super().__init__(master)
 
     def propertieFrame(self, nb, sensors, aansturingen):
+        self.x= [1,2,3,4]
+
+
+        if len(aansturingen)==0:
+            pass
+        else:
+            self.x=[]
+            for instantie in aansturingen:
+                self.x.append(instantie.name)
+                
         rolluiktitel = Label(self, text="Selecteer een rolluik:")
-        rolluiktitel.grid(row = 0, column = 0, columnspan = 25, padx = 1, pady = 5, sticky = 'w')
+        rolluiktitel.grid(row = 0, column = 0, columnspan = 1, padx = 1, pady = 5)
         rolluiktitel.config(font=("Times new roman", 14))
         #sensors.append("moooi")
         hernoemtitel = Label(self, text="hernoem de module:")
-        hernoemtitel.grid(row = 2, column = 0, columnspan = 25, padx = 1, pady = 5, sticky = 'w')
+        hernoemtitel.grid(row = 2, column = 0, columnspan = 1,pady = 5)
         hernoemtitel.config(font=("Times new roman", 12))
         
         self.var = StringVar(self)
         self.var.set("Selecteer motor module")
-        self.w = OptionMenu(self, self.var, sensors)### hier moet motormodulelijst komen
-        self.w.grid(row = 1, column = 0, columnspan = 40, padx = 1, pady = 1, sticky = 'w')
+        self.selecteermotor = OptionMenu(self, self.var, *self.x)### hier moet motormodulelijst komen
+        self.selecteermotor.grid(row = 1, column = 0, columnspan = 1, padx = 1, pady = 1)
         
-        
+        self.hernoemen = Entry(self)
+        self.hernoemen.grid(row = 2, column= 12 , columnspan = 1, padx = 1, pady = 1)
+
+        self.sensortitel = Label(self, text= 'Grenswaarde:')
+        self.sensortitel.grid(row = 0, column = 80, columnspan = 1, padx = 1, pady = 5)
+        self.sensortitel.config(font=("Times new roman", 14))
+
+        self.sensortitel = Label(self, text= 'gebruiken:')
+        self.sensortitel.grid(row = 0, column = 100, columnspan = 1, padx = 1, pady = 5)
+        self.sensortitel.config(font=("Times new roman", 14))
+
+        self.variabelevoorrijenaanpassen = 1 #wordt gebruikt om row aan te passen per sensor
+        """
+        for sensorinstantie in sensors:
+
+            
+
+            self.sensortitel = Label(self, text= str(sensorinstantie.name))
+            self.sensortitel.grid(row = self.variabelevoorrijenaanpassen, column = 60, columnspan = 1, padx = 1, pady = 5)
+            self.sensortitel.config(font=("Times new roman", 14))
+    
+
+            self.sensorwaarde = Entry(self)
+            self.sensorwaarde.grid(row=self.variabelevoorrijenaanpassen, column= 80, columnspan = 1, padx = 1, pady = 1)
+            
+
+            self.checkbox = Checkbutton(self)
+            self.checkbox.grid(row=self.variabelevoorrijenaanpassen, column= 100 , columnspan = 1, padx = 1, pady = 1)
+
+            self.variabelevoorrijenaanpassen +=1
+            """
+
+        def sensorlijst(self, sensors):
+            for sensorinstantie in sensors:
+
+            self.sensortitel = Label(self, text= str(sensorinstantie.name))
+            self.sensortitel.grid(row = self.variabelevoorrijenaanpassen, column = 60, columnspan = 1, padx = 1, pady = 5)
+            self.sensortitel.config(font=("Times new roman", 14))
+    
+
+            self.sensorwaarde = Entry(self)
+            self.sensorwaarde.grid(row=self.variabelevoorrijenaanpassen, column= 80, columnspan = 1, padx = 1, pady = 1)
+            
+
+            self.checkbox = Checkbutton(self)
+            self.checkbox.grid(row=self.variabelevoorrijenaanpassen, column= 100 , columnspan = 1, padx = 1, pady = 1)
+
+            self.variabelevoorrijenaanpassen +=1
+
+            
+
+        """
         for x in sensors:
             
             grenswaardelabel = Label(self, text="Grenswaarde")
@@ -81,7 +142,7 @@ class Properties(Frame):
             s7box = Checkbutton(self)
             s7box.grid(row = 61, column=280 , columnspan = 40, padx = 1, pady = 1, sticky = 'n')
             print(x)
-
+        """
         nb.add(self, text='Properties')
         
         print(sensors)
