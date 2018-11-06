b87e069ef9f571f52ba507c0d9913474d92d9271
Mike Clarke <mikec655@gmail.com>
2018-11-06 08:14:17 +0100

    Home frame laat knoppen zien voor elke afzonderlijke aansturing

--- Centrale/centrale/home.py
+++ Centrale/centrale/home.py
@@ -1,14 +1,36 @@
-from tkinter import Button
+from tkinter import Button, Label
 from myframe import MyFrame
 
 class Home(MyFrame):
     def __init__(self, nb, aansturingen):
         super().__init__(nb, "Home")
-        self.aansturingen = aansturingen
+        self.aansturingen = aansturingen.copy()
+        self.widgets = []
+
+        label = Label(self, text="Alle Schermen")
+        label.grid()
         inrol_button = Button(self, text="Inrollen", command=self.inrollen)
-        uitrol_button = Button(self, text="Uitrollen", command=self.uitrollen)
         inrol_button.grid()
+        uitrol_button = Button(self, text="Uitrollen", command=self.uitrollen)
         uitrol_button.grid()
+
+    def update(self, aansturingen):
+        if aansturingen == self.aansturingen:
+            return
+        for widget in self.widgets:
+            widget.destroy()
+        for aansturing in aansturingen:
+            label = Label(self, text=aansturing.name)
+            self.widgets.append(label)
+            label.grid()
+            inrol_button = Button(self, text="Inrollen", command=aansturing.inrollen)
+            self.widgets.append(inrol_button)
+            inrol_button.grid()
+            uitrol_button = Button(self, text="Uitrollen", command=aansturing.uitrollen)
+            self.widgets.append(uitrol_button)
+            uitrol_button.grid()
+        self.aansturingen = aansturingen.copy()
+        
 
     def uitrollen(self):
         for aansturing in self.aansturingen:
--- Centrale/centrale/main.py
+++ Centrale/centrale/main.py
@@ -26,6 +26,7 @@ class Application(Tk):
         self.title("Application")
 
         # control variables
+        self.alive = True
         self.loggedin = False
         self.sensors = []
         self.sensorsWithoutGraph = []
@@ -47,7 +48,8 @@ class Application(Tk):
         while True:
             try:
                 self.update()
-                self.check_for_devices()
+                #self.check_for_devices()
+                self.frames['Home'].update(self.aansturingen)
                 for sensor in self.sensorsWithoutGraph:
                     self.frames[sensor.name] = Graph(sensor, self.nb)
                     self.sensorsWithoutGraph.remove(sensor)
@@ -57,8 +59,10 @@ class Application(Tk):
                 try:
                     sys.exit(1)
                 except SystemExit: 
+                    self.alive = False
                     for sensor in self.sensors:
                         sensor.stop()
+
                     print("programma afgesloten")
                 break
 
@@ -81,28 +85,29 @@ class Application(Tk):
                         print('FrameDestroyError :(')
 
     def check_for_devices(self):
-        available_ports = list_ports.comports()
-        for port in available_ports:
-            # Als sensor niet in de sensors staat voeg toe
-            if port.device not in [sensor.port for sensor in self.sensors] and \
-            port.device not in [aansturing.port for aansturing in self.aansturingen]:
-                threadName = port.device + "-Thread"
-                if threadName not in [thread.name for thread in threading.enumerate()]:
-                    threading.Thread(name=threadName, target=self.init_device, args=(port.device, port.serial_number)).start()
-        for sensor in self.sensors:
-            # Als sensor niet meer aangesloten staat verwijder van sensor
-            if sensor.port not in [port.device for port in available_ports]:
-                self.frames[sensor.name].deleteFrame()
-                del self.frames[sensor.name]
-                sensor.stop()
-                self.sensors.remove(sensor)
-        for aansturing in self.aansturingen:
-            # Als aansturing niet meer aangesloten staat verwijder aansturing
-            if aansturing.port not in [port.device for port in available_ports]:
-                self.aansturingen.remove(aansturing)
-        for other_port in self.other_com_ports:
-            if other_port not in [port.device for port in available_ports]:
-                self.other_com_ports.remove(other_port)
+        while self.alive:
+            available_ports = list_ports.comports()
+            for port in available_ports:
+                # Als sensor niet in de sensors staat voeg toe
+                if port.device not in [sensor.port for sensor in self.sensors] and \
+                port.device not in [aansturing.port for aansturing in self.aansturingen]:
+                    threadName = port.device + "-Thread"
+                    if threadName not in [thread.name for thread in threading.enumerate()]:
+                        threading.Thread(name=threadName, target=self.init_device, args=(port.device, port.serial_number)).start()
+            for sensor in self.sensors:
+                # Als sensor niet meer aangesloten staat verwijder van sensor
+                if sensor.port not in [port.device for port in available_ports]:
+                    self.frames[sensor.name].deleteFrame()
+                    del self.frames[sensor.name]
+                    sensor.stop()
+                    self.sensors.remove(sensor)
+            for aansturing in self.aansturingen:
+                # Als aansturing niet meer aangesloten staat verwijder aansturing
+                if aansturing.port not in [port.device for port in available_ports]:
+                    self.aansturingen.remove(aansturing)
+            for other_port in self.other_com_ports:
+                if other_port not in [port.device for port in available_ports]:
+                    self.other_com_ports.remove(other_port)
                 
     def init_device(self, comport, id):
         sleep(1)
